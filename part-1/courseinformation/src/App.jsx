2219034ad35f200d32d4f91c022571c5b4814c5c
vpup1pu <ainhoabelenvargas@gmail.com>
2024-08-07 17:01:57 -0300

    part 1d done

---
+++ part-1/courseinformation/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+// This is the right place to define a component
+const Display = props => <div>{props.value}</div>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+// Do not define components inside another component.The biggest 
+//problems are because React treats a component defined inside of 
+//another component as a new component in every render. This makes 
+//it impossible for React to optimize the component.
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+    </div>
+  )
+}
+
+export default App
